55e962313e375edbe9dd86d8f42c9c7710144b9e
genivalrm <genival_rm1@hotmail.com>
2018-05-25 14:05:54 -0300

    atualização das permissões

--- resources/views/curriculas.blade.php
+++ resources/views/curriculas.blade.php
@@ -1,14 +1,14 @@
 <section class="mdl-layout__tab-panel is-active" id="scroll-tab-1">
     <div class="page-content">
         <div class="mdl-grid pd-tb-60 not-archived-section">
-            @include('card-section', ['profiles' => $not_archived])                  
+            @include('card-section', ['profiles' => $not_archived])
         </div>
     </div>      
 </section>
 <section class="mdl-layout__tab-panel" id="scroll-tab-2">
     <div class="page-content">
         <div class="mdl-grid pd-tb-60 archived-section">
-            @include('card-section', ['profiles' => $archived])                
+            @include('card-section', ['profiles' => $archived])
         </div>
     </div> 
 </section>
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,13 @@
 Route::get('auth/logout', 'LoginController@logout');
 Route::get('auth', 'LoginController@index');
 Route::post('auth', 'LoginController@authenticate');
-//Route::get('curriculum/list/{archived}', 'CurriculumController@list')->middleware('auth');
-//Route::get('curriculum/notarchived', 'CurriculumController@listNotArchived')->middleware('auth');
+
 Route::post('curriculum/{id}/tag/delete', 'CurriculumController@deleteTag')->middleware('auth');
 Route::post('curriculum/{id}/tag', 'CurriculumController@insertTag')->middleware('auth');
 Route::get('curriculum/{id}/tag', 'CurriculumController@listTag')->middleware('auth');
 Route::post('curriculum/{id}/rating', 'CurriculumController@updateStar')->middleware('auth');
 Route::post('curriculum/{id}/archive', 'CurriculumController@archive')->middleware('auth');
 Route::post('curriculum/{id}/restore', 'CurriculumController@restore')->middleware('auth');
-// Route::delete('curriculum/{id}', 'CurriculumController@delete')->middleware('auth');
 Route::resource('curriculum', 'CurriculumController')->middleware('auth');
+
 Route::resource('/', 'HomeController');
