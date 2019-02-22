719624cd89f0547ef2248f182491b6119e5f43b0
denisson leal <denissonleal@gmail.com>
2019-02-22 10:59:19 -0300

    corrige importacao de plugins

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,17 +17,21 @@ mix.js('resources/assets/js/insert.js', 'public/js/insert.js');
 mix.js('resources/assets/js/list.js', 'public/js/list.js');
 mix.js('resources/assets/js/mask-tel.js', 'public/js/mask-tel.js');
 mix.js('resources/assets/js/search.js', 'public/js/search.js');
-mix.copy(['node_modules/jquery/dist/jquery.min.js', 'node_modules/jquery-mask-plugin/dist/jquery.mask.min.js',
-	'node_modules/jquery-bar-rating/dist/jquery.barrating.min.js', 'node_modules/dialog-polyfill/dialog-polyfill.js'], 'public/lib/js');
-//   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.copy([
+	'node_modules/jquery/dist/jquery.min.js',
+	'node_modules/jquery-mask-plugin/dist/jquery.mask.min.js',
+	'node_modules/jquery-bar-rating/dist/jquery.barrating.min.js',
+	'node_modules/dialog-polyfill/dialog-polyfill.js',
+	'node_modules/material-design-lite/material.min.js',
+], 'public/lib/js');
 mix.styles('resources/assets/css/insert.css', 'public/css/insert.css');
 mix.styles('resources/assets/css/list.css', 'public/css/list.css');
 mix.styles('resources/assets/css/all.css', 'public/css/all.css');
 mix.styles('resources/assets/css/utils.css', 'public/css/utils.css');
 mix.styles('resources/assets/css/login.css', 'public/css/login.css').version();
-mix.copy(['node_modules/dialog-polyfill/dialog-polyfill.css','node_modules/jquery-bar-rating/dist/themes/fontawesome-stars.css'], 'public/lib/css');
-
-// mix.browserSync({
-// 	proxy: '192.168.99.100:8080'
-// });
+mix.copy([
+	'node_modules/dialog-polyfill/dialog-polyfill.css',
+	'node_modules/jquery-bar-rating/dist/themes/fontawesome-stars.css',
+	'node_modules/material-design-lite/material.min.css',
+], 'public/lib/css');
 
